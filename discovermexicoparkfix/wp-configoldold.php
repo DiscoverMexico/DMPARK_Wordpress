cfb97d869415b67ad30525145908760b4b301dd3
Yamir <yamirsuarez@gmail.com>
2018-12-19 15:45:53 -0500

    Respaldo DM Wordpress

---
+++ discovermexicoparkfix/wp-configoldold.php
@@ -0,0 +1,48 @@
+<?php
+
+define('FS_METHOD', 'direct');
+
+
+// ** MySQL settings ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'db623204765');
+
+/** MySQL database username */
+define('DB_USER', 'dbo623204765');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'DpQgPXPegqXLblsITiAl');
+
+/** MySQL hostname */
+define('DB_HOST', 'db623204765.db.1and1.com');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',         '9y8+.TJ;8Jy7 -dcHS|fTA{[/?|/`?[7%]ks|v},x&FP;3-s`O6t(%3b<0[cH1zW');
+define('SECURE_AUTH_KEY',  '@UjE_.NUAva]W|}DEFX~%|Ff:1HCZu{q$K[X|s$U%h)fA/%tHad9sDN0^;<bm-9.');
+define('LOGGED_IN_KEY',    'GD9p.<#ak)#$A)66D;=+ s{kD-,6s8hb6{QZ#puuaQcGwbogXfX>u|QtaOnQc}+o');
+define('NONCE_KEY',        '%3X6MYU}A+_ZX~yS@S}2gwsI0s2P*.X#m*,U=Mrul}XSteLK.3AD=+&C:sM6(Jxs');
+define('AUTH_SALT',        'X%7]qr#HTR*6Ia!Ci4%B^7Vw|w )*NY-<__hFNK2vIB7;Va[p7M]_|F:?Qn8c.8?');
+define('SECURE_AUTH_SALT', '*+)sI0|)Nz>-T)V0OR J4A%0Yf/jARLi-A7br2+t2Q~AEc|V1R0SbWd=0/i!P4~p');
+define('LOGGED_IN_SALT',   '~>ZN6[0!DV-BP?ENrX!X{UB[d>7s5`:?|u(^SottrKyyy34J_;l^Tl-$I[jvw++x');
+define('NONCE_SALT',       '|=modeEmI.jb?eBX)+`.+J-!IC1{aP1GL}B01{pHv;#64+Qxh3IsG$<57O3*8.8?');
+
+
+$table_prefix = 'htXJkgPp';
+
+
+
+
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ discovermexicoparkfix/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:")uS7gsJ9tz#iA|8J{5QB/]4j,fIXZ=0I2:O0RY5=LV]F5 +Er5*ep{=,_Gf}E=Gf";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3292;}
